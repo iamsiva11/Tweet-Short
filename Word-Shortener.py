8254f6d63bc0d59e55850e20096bcc49b808128b
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2016-08-08 23:32:21 +0530

    Cleaned Spaces

--- Word-Shortener.py
+++ Word-Shortener.py
@@ -10,7 +10,6 @@ and Shopian districts of south Kashmir."""
 
 #removes vowels except if first character is a vowel
 def put_in_a_list(sentence):  
-	
 	words=sentence.split()
 	#remove vowels , if they are not the first characters in the string
 	words = [w.replace('e', '').replace('a','').replace('i','').
@@ -20,16 +19,13 @@ def put_in_a_list(sentence):
 	return words
 
 def remove_rep_consonants(words):
-	
 	#Removing repeated Consonants
 	words=[	re.sub(r'(\w)\1+', r'\1', e ) for e in words]
 	rep_consonants_removed =' '.join(map(str, words))
 	return rep_consonants_removed
 
 def getme_proper_Nouns(text_data):
-	
 	tokenised_text = nltk.word_tokenize(text_data)
-	#text/sentence.split()
 	tagged_set = nltk.pos_tag(tokenised_text)
 	propernouns = [word for word,pos in tagged_set if pos == 'NNP']
 	return propernouns
@@ -42,14 +38,3 @@ def words_compressed_stat(text2):
 
 print remove_rep_consonants(put_in_a_list(text2))
 print words_compressed_stat(text2)
-
-
-
-
-
-
-
-
-
-
-
