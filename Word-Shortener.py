c8a79c330472cd55440c9bde2c0d2ff0ec597179
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2016-08-08 23:51:34 +0530

    Cleaned,removed whitespaces

--- Word-Shortener.py
+++ Word-Shortener.py
@@ -12,14 +12,15 @@ and Shopian districts of south Kashmir."""
 def put_in_a_list(sentence):  
 	words=sentence.split()
 	#remove vowels , if they are not the first characters in the string
-	words = [w.replace('e', '').replace('a','').replace('i','').
-	replace('o','').replace('u','') if (w[0] not in ['a','e','i','o','u'])
-	else w[:1]+w[1:].replace('e', '').replace('a','').replace('i','').
+	words = [w.replace('e', '').replace('a','').replace('i','').\
+	replace('o','').replace('u','') if (w[0] not in ['a','e','i','o','u'])\
+	else w[:1]+w[1:].replace('e', '').replace('a','').replace('i','').\
 	replace('o','').replace('u','') for w in words]
+
 	return words
 
+#Removing repeated Consonants
 def remove_rep_consonants(words):
-	#Removing repeated Consonants
 	words=[	re.sub(r'(\w)\1+', r'\1', e ) for e in words]
 	rep_consonants_removed =' '.join(map(str, words))
 	return rep_consonants_removed
@@ -34,7 +35,7 @@ def words_compressed_stat(text2):
 	orginal_str=text2
 	result_str = remove_rep_consonants(put_in_a_list(text2))
 	num = 1-(len(result_str)/len(orginal_str))
-	print "Compressed String by ", int(num*100) ,"percent"
+	print "Compressed the text by", int(num*100) ,"percent"
 
 print remove_rep_consonants(put_in_a_list(text2))
-print words_compressed_stat(text2)
+words_compressed_stat(text2)
