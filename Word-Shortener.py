76a746b43a9175a73d3046c46b9150a693f19772
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2016-08-08 23:30:04 +0530

    added words_compressed_stat function

--- Word-Shortener.py
+++ Word-Shortener.py
@@ -1,14 +1,12 @@
+from __future__ import division
 from collections import OrderedDict
 import re
 import nltk
 
+#Test Strings
 text="Learn the basics of web design"
-# text="irrespective of its consequences"
-# text2="""Clashes broke out between protesters and security forces in Anantnag and Shopian districts of 
-# south Kashmir."""
-#On Friday, three civilians were killed and more than 150 injuredin clashes across
-#the valley."""
-
+text2="""Clashes broke out between protesters and security forces in Anantnag
+and Shopian districts of south Kashmir."""
 
 #removes vowels except if first character is a vowel
 def put_in_a_list(sentence):  
@@ -28,7 +26,6 @@ def remove_rep_consonants(words):
 	rep_consonants_removed =' '.join(map(str, words))
 	return rep_consonants_removed
 
-
 def getme_proper_Nouns(text_data):
 	
 	tokenised_text = nltk.word_tokenize(text_data)
@@ -37,9 +34,22 @@ def getme_proper_Nouns(text_data):
 	propernouns = [word for word,pos in tagged_set if pos == 'NNP']
 	return propernouns
 
+def words_compressed_stat(text2):
+	orginal_str=text2
+	result_str = remove_rep_consonants(put_in_a_list(text2))
+	num = 1-(len(result_str)/len(orginal_str))
+	print "Compressed String by ", int(num*100) ,"percent"
+
+print remove_rep_consonants(put_in_a_list(text2))
+print words_compressed_stat(text2)
 
 
-print remove_rep_consonants(put_in_a_list(text))
 
-#print remove_rep_consonants(put_in_a_list(text2))
+
+
+
+
+
+
+
 
